5c042ca0d512f8b1b65636fdf4bce8f7a9b977a6
Maadeha shaikh <maadeham@gmail.com>
2024-09-08 04:21:06 +0500

    updated !

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
-var greeting = document.getElementById("greeting");
-var button = document.getElementById("changeText");
-button.addEventListener("click", function () {
-   greeting.style.color = "purple";
-   
+var infotoggle = document.getElementById("infotoggle");
+var infoContent = document.getElementById("infoContent");
+var edutoggle = document.getElementById("edutoggle");
+var eduContent = document.getElementById("eduContent");
+var skilltoggle = document.getElementById("skilltoggle");
+var skillcontent = document.getElementById("skillcontent");
+var exptoggle = document.getElementById("exptoggle");
+var expcontent = document.getElementById("expcontent");
+infotoggle === null || infotoggle === void 0 ? void 0 : infotoggle.addEventListener("click", function () {
+    if (infotoggle.innerText === "HIDE") {
+        infoContent.style.display = "none";
+        infotoggle.innerText = "SHOW";
+    }
+    else {
+        infotoggle.innerText = "HIDE";
+        infoContent.style.display = "block";
+    }
+});
+edutoggle === null || edutoggle === void 0 ? void 0 : edutoggle.addEventListener("click", function () {
+    if (edutoggle.innerText === "HIDE") {
+        eduContent.style.display = "none";
+        edutoggle.innerText = "SHOW";
+    }
+    else {
+        edutoggle.innerText = "HIDE";
+        eduContent.style.display = "block";
+    }
+});
+skilltoggle === null || skilltoggle === void 0 ? void 0 : skilltoggle.addEventListener("click", function () {
+    if (skilltoggle.innerText === "HIDE") {
+        skillcontent.style.display = "none";
+        skilltoggle.innerText = "SHOW";
+    }
+    else {
+        skilltoggle.innerText = "HIDE";
+        skillcontent.style.display = "block";
+    }
+});
+exptoggle === null || exptoggle === void 0 ? void 0 : exptoggle.addEventListener("click", function () {
+    if (exptoggle.innerText === "HIDE") {
+        expcontent.style.display = "none";
+        exptoggle.innerText = "SHOW";
+    }
+    else {
+        exptoggle.innerText = "HIDE";
+        expcontent.style.display = "block";
+    }
 });
--- app.ts
+++ app.ts
@@ -1,6 +1,53 @@
-const greeting = document.getElementById("greeting") as HTMLHeadingElement;
-const button = document.getElementById("changeText") as HTMLButtonElement;
+const infotoggle = document.getElementById("infotoggle");
+var infoContent = document.getElementById("infoContent");
+const edutoggle = document.getElementById("edutoggle");
+var eduContent = document.getElementById("eduContent");
+const skilltoggle = document.getElementById("skilltoggle");
+var skillcontent = document.getElementById("skillcontent");
+const exptoggle = document.getElementById("exptoggle");
+var expcontent = document.getElementById("expcontent");
 
-button.addEventListener("click", () => {
-  greeting.textContent = "You've clicked the button!";
-});
+
+infotoggle?.addEventListener("click",()=>{
+  if(infotoggle.innerText === "HIDE"){
+    infoContent.style.display ="none";
+  infotoggle.innerText="SHOW";
+  }
+  else{
+    infotoggle.innerText="HIDE";
+    infoContent.style.display ="block";
+  }
+})
+
+edutoggle?.addEventListener("click",()=>{
+  if(edutoggle.innerText === "HIDE"){
+  eduContent.style.display ="none";
+  edutoggle.innerText="SHOW";
+  }
+  else{
+    edutoggle.innerText="HIDE";
+    eduContent.style.display ="block";
+  }
+})
+
+skilltoggle?.addEventListener("click",()=>{
+  if(skilltoggle.innerText === "HIDE"){
+  skillcontent.style.display ="none";
+  skilltoggle.innerText="SHOW";
+  }
+  else{
+    skilltoggle.innerText="HIDE";
+    skillcontent.style.display ="block";
+  }
+})
+
+exptoggle?.addEventListener("click",()=>{
+  if(exptoggle.innerText === "HIDE"){
+  expcontent.style.display ="none";
+  exptoggle.innerText="SHOW";
+  }
+  else{
+    exptoggle.innerText="HIDE";
+    expcontent.style.display ="block";
+  }
+})
